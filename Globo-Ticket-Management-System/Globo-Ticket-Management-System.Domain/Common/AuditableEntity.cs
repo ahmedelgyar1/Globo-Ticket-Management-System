2eef04bd955a8b9614bbd723cd19d95eba20405d
Ahmed Elgyar <elgyarahmed71@gmail.com>
2025-11-18 15:19:04 +0200

    feat: initialize solution structure (API/Core/Infrastructure) and add domain entities with AuditableEntity

---
+++ Globo-Ticket-Management-System/Globo-Ticket-Management-System.Domain/Common/AuditableEntity.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Globo_Ticket_Management_System.Domain.Common
+{
+    public class AuditableEntity
+    {
+        public string ? CreatedBy { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+        
+        public string ? LastModifiedBy { get; set; }
+        
+        public DateTime LastModifiedAt { get; set; }
+    }
+}
---
+++ Globo-Ticket-Management-System/Globo-Ticket-Management-System.Domain/Entities/Category.cs
@@ -0,0 +1,16 @@
+﻿using Globo_Ticket_Management_System.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Globo_Ticket_Management_System.Domain.Entities
+{
+    public class Category: AuditableEntity
+    {
+        public Guid CategoryId { get; set; }
+        public string Name { get; set; }
+        public ICollection<Event> Events { get; set; }
+    }
+}
